97ac25d910a60be374d013f2e24ff3e4dc781f8c
oudra brahim <brahimbra12356@gmail.com>
2024-08-06 18:35:46 +0100

    "Updated clients index view"

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -23,27 +23,14 @@
     </header>
 
     <main class="">
-        <div class="container-fluid"> <!-- استخدم container-fluid لتغطية العرض الكامل -->
-            <h1 class="h3 mb-3">العملاء</h1>
+        <div class="container-fluid"> 
+            <h1 class="h3 mb-3">لائحة الزبائن</h1>
             <div class="row">
-                <div class="col-xl-12"> <!-- استخدم col-xl-12 لعرض الجدول في الصفحة بالكامل -->
+                <div class="col-xl-12">
                     <div class="card">
-                        <div class="card-header pb-0">
-                            <div class="card-actions float-right">
-                                <div class="dropdown show">
-                                    <a href="#" data-toggle="dropdown" data-display="static">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal align-middle">
-                                            <circle cx="12" cy="12" r="1"></circle>
-                                            <circle cx="19" cy="12" r="1"></circle>
-                                            <circle cx="5" cy="12" r="1"></circle>
-                                        </svg>
-                                    </a>
-                                </div>
-                            </div>
-                            <h5 class="card-title mb-0">العملاء</h5>
-                        </div>
-                        <div class="card-body p-0"> <!-- إزالة الهوامش الداخلية -->
-                            <table class="table table-striped w-100"> <!-- استخدم w-100 لجعل الجدول يملأ العرض بالكامل -->
+
+                        <div class="card-body p-0">
+                            <table class="table table-striped w-100">
                                 <thead>
                                     <tr>
                                         <th>#</th>
